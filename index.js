766575072b55e9b81c3c842f94816b50e65dd042
Kanitkorn S <k.sujautra@gmail.com>
2015-10-12 17:57:21 +0700

    Save result as CSV for db import

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var x = Xray()
 
 var baseURLPrefix = "http://www.thaischool.in.th/sitemap.php?school_area=&province_id=",
     baseURLSuffix = "&schooltype_id=&txtsearch=",
-    provinces = require('./provinces.json')
+    provinces = require('./provinces.json'),
+    province_id = 1,
+    school_id = 1
 
 function appendObject(obj){
   var resultFile = fs.readFileSync('./result/result.json')
@@ -21,6 +23,16 @@ function appendArray(arr){
   Array.prototype.push.apply(result, obj);
   var resultJSON = JSON.stringify(result)
   fs.writeFileSync('./result/schools.json', resultJSON)
+}
+
+function appendCSV(province, arr){
+  var _province_id = province_id
+  province_id += 1
+  fs.appendFile('provinces.csv', _province_id + ',' + province + '\n')
+  arr.map(function(school){
+    fs.appendFile('schools.csv', school_id + ',' + school + ',' + _province_id + '\n')
+    school_id += 1
+  })
 }
 
 provinces.map(function(province){
