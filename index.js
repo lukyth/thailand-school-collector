06f024cbc3f83bb873420d759e9df3213681ad1a
Kanitkorn S <k.sujautra@gmail.com>
2015-10-22 20:42:13 +0700

    Lint index.js file using eslint and fix some bug

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 var Xray = require('x-ray'),
-    fs = require('fs')
-
-var x = Xray()
-
-var baseURLPrefix = "http://www.thaischool.in.th/sitemap.php?school_area=&province_id=",
-    baseURLSuffix = "&schooltype_id=&txtsearch=",
+    fs = require('fs'),
+    x = Xray(),
+    baseURLPrefix = 'http://www.thaischool.in.th/sitemap.php?school_area=&province_id=',
+    baseURLSuffix = '&schooltype_id=&txtsearch=',
     provinces = require('./provinces.json'),
     province_id = 1,
     school_id = 1
 
 function appendObject(obj){
-  var resultFile = fs.readFileSync('./result/result.json')
-  var result = JSON.parse(resultFile)
+  var resultFile = fs.readFileSync('./result/result.json'),
+      result = JSON.parse(resultFile),
+      resultJSON
   result.push(obj)
-  var resultJSON = JSON.stringify(result)
+  resultJSON = JSON.stringify(result)
   fs.writeFileSync('./result/result.json', resultJSON)
 }
 
 function appendArray(arr){
-  var resultFile = fs.readFileSync('./result/schools.json')
-  var result = JSON.parse(resultFile)
-  Array.prototype.push.apply(result, obj);
-  var resultJSON = JSON.stringify(result)
+  var resultFile = fs.readFileSync('./result/schools.json'),
+      result = JSON.parse(resultFile),
+      resultJSON
+  Array.prototype.push.apply(result, arr)
+  resultJSON = JSON.stringify(result)
   fs.writeFileSync('./result/schools.json', resultJSON)
 }
 
 function appendCSV(province, arr){
   var _province_id = province_id
   province_id += 1
-  fs.appendFile('provinces.csv', _province_id + ',' + province + '\n')
+  fs.appendFile('./result/provinces.csv', _province_id + ',' + province + '\n')
   arr.map(function(school){
-    fs.appendFile('schools.csv', school_id + ',' + school + ',' + _province_id + '\n')
+    fs.appendFile('./result/schools.csv', school_id + ',' + school + ',' + _province_id + '\n')
     school_id += 1
   })
 }
@@ -43,5 +43,6 @@ provinces.map(function(province){
         schools: arr
       })
       appendArray(arr)
+      appendCSV(province['name'], arr)
     })
 })
